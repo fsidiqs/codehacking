5dc87ce8d5e2ea2efed6bd861305f87d6ed5184b
Fajar <fsidiqs@hotmail.co.id>
2017-09-09 10:04:41 +0700

    Users section working except security

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -17,7 +17,7 @@
 
      <div class="form-group">
       {!! Form::label('role_id', 'Role:')!!}
-      {!! Form::select('role_id',  $roles, null, ['class'=>'form-control']) !!}
+      {!! Form::select('role_id',  [null=>'Please Select'] +$roles, null, ['class'=>'form-control']) !!}
      </div>
 
      <div class="form-group">
